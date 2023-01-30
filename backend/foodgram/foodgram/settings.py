8ae251a99150ebc1246b8768006da9a06c11808c
Oleg Zagryazkin <zagryazkinoleg@yandex.ru>
2023-01-30 17:32:41 +0300

    final 22

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -24,9 +24,8 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-secret-key')
 # 'django-insecure-&gt9#f9#on&w#gxiiooka(z#kskyz97y94j%a&qf(()3vbgdai'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
