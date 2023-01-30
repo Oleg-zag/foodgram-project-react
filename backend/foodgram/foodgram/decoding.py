1e6eb1e8a8114ddd5494afccbd40042353d304c8
Oleg Zagryazkin <zagryazkinoleg@yandex.ru>
2023-01-30 19:55:18 +0300

    final 29

--- backend/foodgram/foodgram/decoding.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-path = 'D:/Dev/foodgram-project-react/data/ingredients.csv'
-
-with open(path, encoding="UTF8") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
